a2f15579ff4bfb2517184f6967d70d9071544ad5
Lawrence MacGuire <leprechaun@gmail.com>
2013-10-31 01:02:45 +0800

    Adding a simpler TrackNumberInTitle(Smart)
    it creeps into Track.Title until the substring can't be parsed as an int. It strips that, and a few chars (configurable) and sets values

--- bansheefixer/fixes.py
+++ bansheefixer/fixes.py
@@ -42,6 +42,47 @@ class TrackNumberInTitle(Fixer):
         else:
             print(repr(track) + " doesn't have a track number in the title")
 
+class TrackNumberInTitleSmart(Fixer):
+    def __init__(self, strip_chars=".- "):
+        self._chars = strip_chars
+
+
+    def apply_track(self, track):
+        tn = None
+        for i in range(len(track.Title)-1):
+            st = track.Title[0:i+1]
+            try:
+                tn = int(st)
+            except ValueError:
+                break
+
+        # substract on, because the last char made it fail
+        i = i - 0
+
+        if tn is not None:
+            track.Title = track.Title[i:].strip(self._chars)
+            track.TrackNumber = tn
+
+
+class TrackAndDiscNumberInTitle(Fixer):
+    def __init__(self, disc_seperator="-", track_seperator=" "):
+        self._disc_seperator = disc_seperator
+        self._track_seperator = track_seperator
+
+    def apply_track(self, track):
+        tex = track.Title.split(self._track_seperator, 1)
+        if len(tex) == 2:
+            tex[0] = tex[0].split(self._disc_seperator)
+            try:
+                tn = int(tex[0][1])
+                track.DiscNumber = tex[0][0]
+                track.Title = tex[1]
+                track.TrackNumber = tn
+            except ValueError:
+                print(repr(track) + " doesn't have a track number in the title")
+        else:
+            print(repr(track) + " doesn't have a track number in the title")
+
 class StripFromTitle(Fixer):
     def __init__(self, to_strip):
         self._to_strip = to_strip
