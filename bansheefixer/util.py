3ebd04379bcf3129e3876b70a1fefd0ba4eb43aa
Lawrence MacGuire <leprechaun@gmail.com>
2013-11-02 02:14:29 +0800

    Add a TableView class so WorkingList doesn't have to be overloaded
    Table view is configurable via the TableView.add_column() method

--- bansheefixer/util.py
+++ bansheefixer/util.py
@@ -1,5 +1,59 @@
 from bansheefixer.entities import *
 
+class TableView(object):
+    def __init__(self):
+        self.columns = sorted([])
+
+    def add_column(self, caption, function):
+        self.columns.append((caption, function))
+
+    def get_repr(self):
+        s = self
+        def f(iterable):
+            rows = []
+
+            # make the header captions
+            rows.append([])
+            for column in s.columns:
+                rows[0].append(column[0])
+
+            # loop through items and generate column strings
+            lengths = [[] for column in s.columns]
+            row_index = 0
+            for item in iterable:
+                row = []
+                # loop through the columns and call the function ...
+                column_index = 0
+                item.__index__ = row_index
+                for column in s.columns:
+                    # run the function on the row to get a colunm
+                    column_string = str(column[1](item))
+                    row.append(column_string)
+                    lengths[column_index].append(len(column_string))
+
+                    column_index = column_index + 1
+
+                rows.append(row)
+                row_index = row_index + 1
+
+            lengths_reduced = [max(length) for length in lengths]
+
+            print(lengths_reduced)
+
+            lines = []
+
+            header = ["" for c in range(len(lengths_reduced))]
+
+            rows.insert(1, header)
+            for row in rows:
+                padded_row = [str(row[c]).ljust(lengths_reduced[c]) for c in range(len(lengths_reduced))]
+                line = "| " + " | ".join(padded_row) + " |"
+                lines.append(line)
+
+            return "\n".join(lines)
+
+        return f
+
 class Helper(object):
     def __init__(self, db):
         self.db = db
@@ -38,38 +92,11 @@ class WorkingList(list):
         for t in self:
             t.Year = year
 
-    def __repr__(self):
-        lines = []
+    def set_album(self, album):
+        self.albums = [album]
 
-        lines.append("Artists:")
-        for a in self.artists:
-            lines.append("- #" + str(a.ArtistID) + " " + str(a.Name))
-
-        lines.append("Albums:")
-        for a in self.albums:
-            lines.append("- #" + str(a.AlbumID) + " " + str(a.Title) + " (" + str(a.Year) + ")")
-
-        lines.append("")
-
-        artist_string_length = max([len(str(a.Name)) for a in self.artists])
-        album_string_length = max([len(str(a.Title)) for a in self.albums])
-        title_string_length = max([len(str(t.Title)) for t in self])
-        track_string_length = len(str(len(self)))
-
-        header = "+".ljust(track_string_length+3, "-") + "+".ljust(artist_string_length + 3, "-") + "+".ljust(album_string_length + 3, "-" ) + "+".ljust(title_string_length+6, "-") + "+"
-        lines.append(header)
-        i = 0
         for t in self:
-            line = "| " + str(i).ljust(track_string_length+1) + "|"
-            line = line + " " + str(t.Artist.Name).ljust(artist_string_length) + " |"
-            line = line + " " + str(t.Album.Title).ljust(album_string_length) + " |"
-            line = line + " " + str(t.TrackNumber).ljust(3) + str(t.Title).ljust(title_string_length+1) + "|"
-            lines.append(line)
-            i = i + 1
-
-        lines.append(header)
-
-        return "\n".join(lines)
+            t.Album = album
 
     def sync_tags(self):
         for t in self:
@@ -79,4 +106,6 @@ class WorkingList(list):
                 print(repr(t) + " NOT FOUND")
 
         print("You probably need to run 'session.commit()'")
+
+
 
